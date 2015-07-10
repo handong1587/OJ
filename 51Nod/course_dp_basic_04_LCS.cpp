c8f9b03701a4903443f662c61b6b31e7180b4484
handong1587 <handong1587@163.com>
2015-07-10 13:42:05 +0800

    update course_dp_basic_04_LCS.cpp

--- 51Nod/course_dp_basic_04_LCS.cpp
+++ 51Nod/course_dp_basic_04_LCS.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
+#define _DEBUG_ 0
 #define _max_(a,b) ((a)>(b)?(a):(b))
 
 typedef enum{TGT,LFT,TOP} FromType;
@@ -8,9 +9,6 @@ char ftype[3][10]={"TGT","LFT","TOP"};
 
 struct LocInfo
 {
-//    bool isTgt;
-//    int prevx;
-//    int prevy;
     FromType ft;
 };
 
@@ -35,13 +33,9 @@ void solve()
 
             }
             else if(s1[i-1]==s2[j-1]){
-                //printf("s1[%d]==s[%d]==%c\n",i,j,s1[i-1]);
                 dp[i][j]=dp[i-1][j-1]+1;
                 maxn=_max_(maxn,dp[i][j]);
 
-                //loc[i][j].isTgt=true;
-                //loc[i][j].prevx=i-1;
-                //loc[i][j].prevy=j-1;
                 loc[i][j].ft=TGT;
 
                 finalx=i;
@@ -52,55 +46,48 @@ void solve()
                 dp[i][j]=_max_(dp[i-1][j],dp[i][j-1]);
                 maxn=_max_(maxn,dp[i][j]);
 
-                //loc[i][j].isTgt=false;
                 if(dp[i-1][j]>dp[i][j-1]){
-                    //loc[i][j].prevx=i-1;
-                    //loc[i][j].prevy=j;
                     loc[i][j].ft=LFT;
                 }
                 else {
-                    //loc[i][j].prevx=i-1;
-                    //loc[i][j].prevy=j;
                     loc[i][j].ft=TOP;
                 }
             }
         }
     }
 
+#if _DEBUG_
     for(int i=0;i<=n1;i++){
         for(int j=0;j<=n2;j++){
             printf("(%s,%s)%s",ftype[loc[i][j].ft],loc[i][j].ft==TGT?"Y":"N",j==n2?"\n":" ");
         }
     }
+#endif
 
     int curx=finalx,cury=finaly;
-    //printf("finalx=%d,finaly=%d\n",finalx,finaly);
     int tgtn=0;
     char revs[1005];
     while(curx>=1&&cury>=1){
         if(loc[curx][cury].ft==TGT){
             revs[tgtn++]=s1[curx-1];
+#if _DEBUG_
             printf("curx=%d,cury=%d,%c\n",curx,cury,s1[curx-1]);
+#endif
         }
-        //int tmpx=loc[curx][cury].prevx;
-        //int tmpy=loc[curx][cury].prevy;
-        //curx=tmpx;
-        //cury=tmpy;
         if(loc[curx][cury].ft==TGT){
             curx--;
             cury--;
         }
         else if(loc[curx][cury].ft==LFT){
             curx--;
-            //cury--;
         }
         else{
             cury--;
         }
+#if _DEBUG_
         printf("curx=%d,cury=%d\n",curx,cury);
+#endif
     }
-    //printf("tgtn=%d\n",tgtn);
-    //printf("revs=%s\n",revs);
     for(int i=0;i<tgtn;i++){
         maxs[i]=revs[tgtn-1-i];
     }
@@ -115,20 +102,23 @@ int main()
     n1=strlen(s1);
     n2=strlen(s2);
 
+#if _DEBUG_
     //printf("n1=%d\n",n1);
     //printf("n2=%d\n",n2);
     printf("%s\n",s1);
     printf("%s\n",s2);
-
+#endif
     solve();
     //printf("%d\n",maxn);
     printf("%s\n",maxs);
 
+#if _DEBUG_
     for(int i=0;i<=n1;i++){
         for(int j=0;j<=n2;j++){
             printf("%d%s",dp[i][j],j==n2?"\n":" ");
         }
     }
+#endif
 
     return 0;
 }
