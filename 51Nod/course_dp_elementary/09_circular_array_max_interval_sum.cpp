d10fa852c0b381c5ae3b3d29f077a0eb8ead7ca7
handong1587 <handong1587@163.com>
2015-07-13 21:40:38 +0800

    add new files

---
+++ 51Nod/course_dp_elementary/09_circular_array_max_interval_sum.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+#define _max_(a,b) ((a)>(b)?(a):(b))
+#define _min_(a,b) ((a)<(b)?(a):(b))
+
+int N;
+long long a[50005];
+long long sum,maxsum,minsum,res;
+
+void solve()
+{
+    //int mid=N/2;
+    int tmpmaxsum=a[0];
+    int tmpendmax=a[0];
+    for(int i=1;i<N;i++){
+        tmpendmax=_max_(tmpendmax,0)+a[i];
+        tmpmaxsum=_max_(tmpmaxsum,tmpendmax);
+    }
+
+    int tmpminsum=a[0];
+    int tmpendmin=a[0];
+    for(int i=1;i<N;i++){
+        tmpendmin=_min_(tmpendmax,0)+a[i];
+        tmpminsum=_min_(tmpminsum,tmpendmin);
+    }
+    res=_max_(tmpmaxsum, sum-tmpminsum);
+    res=_max_(0,res);
+}
+
+int main()
+{
+    freopen("09_circular_array_max_interval_sum.txt","r",stdin);
+    scanf("%d",&N);
+
+    sum=0;
+    for(int i=0;i<N;i++){
+        scanf("%lld",&a[i]);
+        sum+=a[i];
+    }
+    solve();
+    printf("%lld\n",res);
+    return 0;
+}
