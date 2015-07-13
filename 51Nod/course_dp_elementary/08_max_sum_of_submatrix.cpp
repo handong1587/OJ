75d3981cc3781306c210f6c1f7ec4874b911a598
handong1587 <handong1587@163.com>
2015-07-13 19:55:14 +0800

    add new files

---
+++ 51Nod/course_dp_elementary/08_max_sum_of_submatrix.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#define _max_(a,b) ((a)>(b)?(a):(b))
+
+int M,N;
+long long mat[505][505];
+long long dp[505];
+long long maxsum;
+
+void solve()
+{
+    maxsum=mat[0][0];
+    for(int i=0;i<M;i++){
+        for(int j=i;j<M;j++){
+            for(int k=0;k<N;k++){
+                dp[k]=(j==i)?mat[i][k]:(dp[k]+mat[j][k]);
+            }
+            long long tmpmax = dp[0];
+            long long endmax = dp[0];
+            for(int k=0;k<N;k++){
+                endmax=_max_(endmax,0)+dp[k];
+                tmpmax=_max_(tmpmax,endmax);
+            }
+            maxsum=_max_(maxsum,tmpmax);
+        }
+    }
+}
+
+int main()
+{
+    freopen("08_max_sum_of_submatrix.txt","r",stdin);
+    scanf("%d %d",&M,&N);
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+            scanf("%lld",&mat[i][j]);
+        }
+    }
+    solve();
+    printf("%lld\n",_max_(0,maxsum));
+
+    return 0;
+}
