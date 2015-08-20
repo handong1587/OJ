827c5301517bb0cab464df2ac01a60d173c616af
handong1587 <handong1587@163.com>
2015-08-20 17:19:33 +0800

    update Optimal_Path.cpp

--- Tosong/Optimal_Path.cpp
+++ Tosong/Optimal_Path.cpp
@@ -8,8 +8,8 @@
 int T,N;
 struct Coord
 {
-	int x;
-	int y;
+    int x;
+    int y;
 };
 Coord co[MAX_N];
 int dist[MAX_N][MAX_N];
@@ -19,112 +19,84 @@ int nVisitedCustomer;
 
 void comp_dist()
 {
-	for (int i = 0; i < N;i++)
-	{
-		for (int j = i+1; j < N;j++)
-		{
-			if (i==j)
-			{
-				continue;
-			}
-			dist[i][j] = dist[j][i] = _abs_(co[i].x - co[j].x) + _abs_(co[i].y - co[j].y);
-		}
-	}
+    for (int i = 0; i < N;i++){
+        for (int j = i+1; j < N;j++){
+            if (i==j){
+                continue;
+            }
+            dist[i][j] = dist[j][i] = _abs_(co[i].x - co[j].x) + _abs_(co[i].y - co[j].y);
+        }
+    }
 }
 
 void init()
 {
-	for (int i = 0; i < MAX_N;i++)
-	{
-		vis[i] = false;
-	}
-	cur_dist = 0;
-	min_dist = 0x3f3f3f3f;
-	nVisitedCustomer = 0;
+    for (int i = 0; i < MAX_N;i++){
+        vis[i] = false;
+    }
+    cur_dist = 0;
+    min_dist = 0x3f3f3f3f;
+    nVisitedCustomer = 0;
 }
 
 void dfs(int cur_costomer,int next_customer)
 {
-	
-	if (cur_dist>min_dist)
-	{
-		return;
-	}
-	if (nVisitedCustomer==N-2)
-	{
-		cur_dist += dist[next_customer][1];
+    if (cur_dist>min_dist){
+        return;
+    }
+    if (nVisitedCustomer==N-2){
+        cur_dist += dist[next_customer][1];
+        min_dist = _min_(min_dist, cur_dist);
+        cur_dist -= dist[next_customer][1];
+        return;
+    }
+    for (int i = 2; i < N;i++){
+        if (vis[i]==false){
+            vis[i] = true;
+            cur_dist += dist[next_customer][i];
+            nVisitedCustomer++;
 
-		min_dist = _min_(min_dist, cur_dist);
+            dfs(next_customer, i);
 
-// 		for (int j = 0; j < nVisitedCustomer+1; j++)
-// 		{
-// 			printf(" ");
-// 		}
-// 		printf("return home: %d->%d, current dist: %d, current total dist: %d\n", next_customer, 1, dist[next_customer][1],cur_dist);
-
-		cur_dist -= dist[next_customer][1];
-		return;
-	}
-	for (int i = 2; i < N;i++)
-	{
-		if (vis[i]==false)
-		{
-			vis[i] = true;
-			cur_dist += dist[next_customer][i];
-			nVisitedCustomer++;
-
-// 			for (int j = 0; j < nVisitedCustomer;j++)
-// 			{
-// 				printf(" ");
-// 			}
-// 			printf("%d contomers: %d->%d, current dist: %d, current total dist: %d\n", nVisitedCustomer, next_customer, i, dist[next_customer][i],cur_dist);
-
-			dfs(next_customer, i);
-
-			vis[i] = false;
-			cur_dist -= dist[next_customer][i];
-			nVisitedCustomer--;
-		}
-	}
+            vis[i] = false;
+            cur_dist -= dist[next_customer][i];
+            nVisitedCustomer--;
+        }
+    }
 }
 
 void solve()
 {
-	for (int i = 2; i < N;i++)
-	{
-		nVisitedCustomer = 0;
-		cur_dist = 0;
+    for (int i = 2; i < N;i++){
+        nVisitedCustomer = 0;
+        cur_dist = 0;
 
-		vis[i] = true;
-		cur_dist += dist[0][i];
-		nVisitedCustomer++;
+        vis[i] = true;
+        cur_dist += dist[0][i];
+        nVisitedCustomer++;
 
-		//printf("%d contomers: %d->%d, current dist: %d, current total dist: %d\n", nVisitedCustomer, 0, i, dist[0][i],cur_dist);
+        dfs(0, i);
 
-		dfs(0, i);
-
-		vis[i] = false;
-		cur_dist -= dist[0][i];
-		nVisitedCustomer--;
-	}
+        vis[i] = false;
+        cur_dist -= dist[0][i];
+        nVisitedCustomer--;
+    }
 }
 
 int main()
 {
-	freopen("Optimal_Path.txt", "r", stdin);
-	scanf("%d\n", &T);
-	for (int t = 0; t < T;t++)
-	{
-		scanf("%d\n", &N);
-		N += 2;
-		for (int i = 0; i < N;i++)
-		{
-			scanf("%d%d", &co[i].x, &co[i].y);
-		}
-		init();
-		comp_dist();
-		solve();
-		printf("#%d %d\n", t + 1,min_dist);		
-	}
-	return 0;
+    freopen("Optimal_Path.txt", "r", stdin);
+    scanf("%d\n", &T);
+    for (int t = 0; t < T;t++){
+        scanf("%d\n", &N);
+        N += 2;
+        for (int i = 0; i < N;i++){
+            scanf("%d%d", &co[i].x, &co[i].y);
+        }
+        init();
+        comp_dist();
+        solve();
+        printf("#%d %d\n", t + 1,min_dist);
+    }
+    return 0;
 }
