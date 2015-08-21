20119e9ea05c53c306741adae2c7836d95a6ad35
handong1587 <handong1587@163.com>
2015-08-21 16:22:00 +0800

    update zoj2432

--- ZOJ/zoj2432_Greatest_Common_Increasing_Subsequence.cpp
+++ ZOJ/zoj2432_Greatest_Common_Increasing_Subsequence.cpp
@@ -1,7 +1,34 @@
 #include <stdio.h>
+#include <string.h>
 
-int T,M1,M2,A1[505],A2[505];
-int L,S[505];
+#define MAXN 505
+
+int T,M1,M2,A1[MAXN],A2[MAXN];
+int f[MAXN][MAXN], dp[MAXN];
+int res[MAXN];
+
+int gcis(int a[], int la, int b[], int lb, int ans[])
+{ // a[1…la], b[1…lb]
+    int i, j, k, mx;
+    memset(f, 0, sizeof(f));
+    memset(dp, 0, sizeof(dp));
+    for(i = 1; i <= la; i++) {
+        memcpy(f[i], f[i-1], sizeof(f[0]));
+        for(k = 0, j = 1; j <= lb; j++) {
+            if(b[j-1] < a[i-1] && dp[j] > dp[k])
+                k = j;
+            if(b[j-1] == a[i-1] && dp[k] + 1 > dp[j]) {
+                dp[j] = dp[k] + 1,
+                f[i][j] = i * (lb + 1) + k;
+            }
+        }
+    }
+    for(mx = 0, i = 1; i <= lb; i++)
+        if(dp[i] > dp[mx]) mx = i;
+    for(i=la*lb+la+mx, j=dp[mx]; j; i=f[i/(lb+1)][i%(lb+1)],j--)
+        ans[j-1] = b[i % (lb + 1) - 1];
+    return dp[mx];
+}
 
 int main()
 {
@@ -10,17 +37,19 @@ int main()
   scanf("%d\n",&T);
   for(int t=1;t<=T;t++){
     scanf("%d",&M1);
-    for(int i=0;i<M1;i++){      
+    for(int i=0;i<M1;i++){
       scanf("%d",&A1[i]);
     }
     scanf("%d",&M2);
-    for(int i=0;i<M2;i++){      
+    for(int i=0;i<M2;i++){
       scanf("%d",&A2[i]);
     }
 
-    printf("%d\n",L);
-    for(int i=0;i<L;i++){
-      printf("%d%s",S[i],i==L-1?"\n":" ");
+    int len = gcis(A1, M1, A2, M2, res);
+
+    printf("%d\n",len);
+    for(int i=0;i<len;i++){
+         printf("%d%s",res[i],i==len-1?"\n":" ");
     }
   }
 
