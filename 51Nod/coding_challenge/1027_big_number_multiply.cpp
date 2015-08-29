5a1de8abcbfa2db23bda9b9cf321a85f60157dd0
handong1587 <handong1587@163.com>
2015-08-29 15:42:23 +0800

    update 1027

--- 51Nod/coding_challenge/1027_big_number_multiply.cpp
+++ 51Nod/coding_challenge/1027_big_number_multiply.cpp
@@ -23,12 +23,43 @@ void init()
 }
 
 void solve()
-{}
+{
+    for(int i=MAXL-1, m=0; i>=MAXL-la; i--, m++){
+        memset(carry, 0, sizeof(carry));
+        for(int j=MAXL-1, k=MAXL*2-1, n=0; j>=0; j--, n++, k--){
+            int v_buf, v_carry;
+            v_buf = (buf_c[k-m] + buf_a[i] * buf_b[j] + carry[k-m]) % 10;
+            v_carry = (buf_c[k-m] + buf_a[i] * buf_b[j] + carry[k-m]) / 10;
+            buf_c[k - m] = v_buf;
+            carry[k - m - 1] = v_carry;
+        }
+    }
+}
+
+void print()
+{
+    bool isPrefixZero = true;
+    for(int i=0; i<MAXL * 2; i++){
+        if(isPrefixZero && buf_c[i]==0){
+            if (i == MAXL* 2 - 1){
+                printf("0\n");
+            }
+            continue;
+        }
+        isPrefixZero = false;
+        printf("%d%s", buf_c[i], i==MAXL*2-1?"\n":"");
+    }
+}
 
 int main()
 {
     freopen("1027_big_number_multiply.txt", "r", stdin);
     scanf("%s",a);
     scanf("%s",b);
+
+    init();
+    solve();
+    print();
+
     return 0;
 }
