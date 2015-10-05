2042f7b4372485b673e9c2c26017dfcb1b319ff7
handong1587 <handong1587@163.com>
2015-10-06 02:16:21 +0800

    checkin 1174

--- 51Nod/coding_challenge/1174_rmq.cpp
+++ 51Nod/coding_challenge/1174_rmq.cpp
@@ -65,9 +65,53 @@ int query_rmq(int a, int b)
 }
 // end of RMQ
 
+// RMQ 2: implemented with DP
+#define MAX_NP 14 // 2^14 > 10000
+int dp[MAXN][MAX_NP];
+
+void construct_dp_rmq(int *lcp, int size_)
+{
+	int cnt = 1;
+	while ((1 << cnt) <= size_)
+	{
+		cnt++;
+	}
+	// compute case: j=0
+	for (int i = 0; i < size_; i++)
+	{
+		dp[i][0] = lcp[i];
+	}
+	// dp[i][j] = maximum of S[i ~ i+2^(j)-1]
+	// dp[i][j] = maximum of S[i ~ i+2^(j-1)-1], S[i+2^(j-1) ~ i+2^(j)-1]
+	// dp[i][j] = maximum of dp[i][j-1],			dp[i+2^(j-1)][j-1]
+	for (int j = 1; (1 << j) <= size_; j++)
+	{
+		for (int i = 0; i + (1 << j) - 1 < size_; i++)
+		{
+			int a = dp[i][j - 1];
+			int b = dp[i + (1 << (j - 1))][j - 1];
+			dp[i][j] = max(dp[i][j - 1], dp[i + (1 << (j - 1))][j - 1]);
+		}
+	}
+}
+
+int query_dp_rmq(int a, int b)
+{
+	int len = b - a + 1;
+	int cnt = 1;
+	while ((1 << cnt) <= len)
+	{
+		cnt++;
+	}
+	cnt--;
+	return max(dp[a][cnt], dp[b - (1 << cnt) + 1][cnt]);
+}
+
+// end of RMQ 2
+
 int main()
 {
-	freopen("51Nod/coding_challenge/1174_rmq.txt", "r", stdin);
+	freopen("51Nod/coding_challenge/1174_rmq_case1.txt", "r", stdin);
 
 	scanf("%d", &N);
 	for (int i = 0; i < N; i++)
@@ -76,13 +120,15 @@ int main()
 	}
 	scanf("%d", &Q);
 
-	construct_rmq(S, N);
+	//construct_rmq(S, N);
+	construct_dp_rmq(S, N);
 
 	for (int q = 0; q < Q; q++)
 	{
 		int i, j;
 		scanf("%d %d", &i, &j);
-		int ans = query_rmq(i, j+1);
+		//int ans = query_rmq(i, j+1);
+		int ans = query_dp_rmq(i, j);
 		printf("%d\n", ans);
 	}
 
